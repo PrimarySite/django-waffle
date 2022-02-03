830b1c90a127c6ca787aee19914bd69049f41319
loghmanb <loghmanb@gmail.com>
2022-02-03 13:58:17 +0000

    uncomment the delete flag model

--- waffle/migrations/0002_delete_flag_model.py
+++ waffle/migrations/0002_delete_flag_model.py
@@ -7,24 +7,26 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('waffle', '0001_initial'),
+        ("waffle", "0001_initial"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='flag',
-            name='groups',
+            model_name="flag",
+            name="groups",
         ),
         migrations.RemoveField(
-            model_name='flag',
-            name='users',
+            model_name="flag",
+            name="users",
         ),
         migrations.AlterField(
-            model_name='switch',
-            name='active',
-            field=models.BooleanField(default=False, help_text='Is this switch active?'),
+            model_name="switch",
+            name="active",
+            field=models.BooleanField(
+                default=False, help_text="Is this switch active?"
+            ),
         ),
-#         migrations.DeleteModel(
-#             name='Flag',
-#         ),
+        migrations.DeleteModel(
+            name="Flag",
+        ),
     ]
